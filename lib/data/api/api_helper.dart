d226833f6a8d1dd2b5ea053a9e716fcf5d377383
clusterDe <dhannydhinata@gmail.com>
2025-06-30 21:31:28 +0700

    created api helper

---
+++ lib/data/api/api_helper.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'api_header.dart';
+import 'api_response.dart';
+
+class ApiHelper {
+  static const String _baseUrl = 'https://gutendex.com';
+
+  static apiBooks({required int page}) async {
+    var url = Uri.parse('$_baseUrl/books/?page=$page');
+
+    var headerPost = apiHeader();
+
+    var response = await http.get(url, headers: headerPost);
+    var statusCode = response.statusCode;
+    var reasonPhrase = response.reasonPhrase;
+
+    var body = response.body;
+    var decode = json.decode(body);
+
+    if (apiResponse(statusCode)) {
+      return decode;
+    } else {
+      throw '$reasonPhrase';
+    }
+  }
+}
