d14be5c645fecf73e9c17c509ee78de2b6a14570
clusterDe <dhannydhinata@gmail.com>
2025-06-30 17:27:43 +0700

    created dashboard screen

---
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key, required this.navigationShell});
+
+  final StatefulNavigationShell navigationShell;
+
+  void _onTap(int index) {
+    navigationShell.goBranch(
+      index,
+      initialLocation: index == navigationShell.currentIndex,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: navigationShell.currentIndex,
+        onTap: _onTap,
+        items: [
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.home_filled),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.favorite),
+            icon: Icon(Icons.favorite_outline),
+            label: 'Likes',
+          ),
+        ],
+      ),
+    );
+  }
+}
