9f72acc0b2e2d76e731556ccb0d027be5b90696f
clusterDe <dhannydhinata@gmail.com>
2025-06-30 17:27:32 +0700

    created likes screen

---
+++ lib/screens/likes/likes_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LikesScreen extends StatelessWidget {
+  static const String routeName = '/likes';
+  const LikesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
