6ecb9809c25eed51aa7ea6e228fb62b47605369c
clusterDe <dhannydhinata@gmail.com>
2025-07-01 23:21:03 +0700

    update home body

--- lib/screens/home/components/home_body.dart
+++ lib/screens/home/components/home_body.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:palm_mobile/screens/books_detail/books_detail_screen.dart';
+import 'package:provider/provider.dart';
 import 'package:palm_mobile/data/constant.dart';
 import 'package:palm_mobile/providers/home/home_provider.dart';
-import 'package:provider/provider.dart';
+import '../../../widget/card_book/card_books.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
@@ -32,27 +35,22 @@ class HomeBody extends StatelessWidget {
               homeProv.refreshBooks();
             },
             child: CustomScrollView(
+              controller: homeProv.scrollCtrlBooks,
               physics: AlwaysScrollableScrollPhysics(),
               slivers: [
                 SliverList.separated(
                   itemCount: itemsBooks.length + (homeProv.hasMore ? 1 : 0),
                   itemBuilder: (context, index) {
                     if (index < itemsBooks.length) {
-                      return Card.filled(
-                        margin: EdgeInsets.symmetric(horizontal: defPadding),
-                        elevation: 4,
-                        borderOnForeground: false,
-                        child: ListTile(
-                          title: Text(itemsBooks[index].title ?? '-'),
-                          subtitle: Wrap(
-                            children: List.generate(
-                              itemsBooks[index].authors!.length,
-                              (index2) => Text(
-                                itemsBooks[index].authors?[index2].name ?? '-',
-                              ),
-                            ),
-                          ),
-                        ),
+                      return CardBooks(
+                        press: () {
+                          context.pushNamed(
+                            BooksDetailScreen.routeName,
+                            extra: itemsBooks[index],
+                          );
+                        },
+                        title: itemsBooks[index].title,
+                        author: itemsBooks[index].authors,
                       );
                     } else {
                       return Center(
@@ -64,7 +62,7 @@ class HomeBody extends StatelessWidget {
                     }
                   },
                   separatorBuilder: (context, index) =>
-                      SizedBox(height: defPadding),
+                      SizedBox(height: defPadding / 2),
                 ),
               ],
             ),
