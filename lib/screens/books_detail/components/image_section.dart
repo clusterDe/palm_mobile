74a245783d8b89b3d716bc4711decbe7e84e37e0
clusterDe <dhannydhinata@gmail.com>
2025-07-01 23:22:15 +0700

    created image section

---
+++ lib/screens/books_detail/components/image_section.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:palm_mobile/providers/books_detail/books_detail_provider.dart';
+import 'package:provider/provider.dart';
+
+import '../../../models/books/components/books_result.dart';
+import '../../../widget/cache_image/cache_image.dart';
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.booksDetailData});
+
+  final BooksResult? booksDetailData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: (context) {
+        if (booksDetailData?.formats?.imageJpeg != null) {
+          return Stack(
+            clipBehavior: Clip.none,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: CacheImage(
+                  url: '${booksDetailData?.formats?.imageJpeg}',
+                ),
+              ),
+              Positioned(
+                bottom: -25,
+                right: 0,
+                child: Consumer<BooksDetailProvider>(
+                  builder: (context, bookDetailProv, _) {
+                    var likes = bookDetailProv.likes;
+                    return IconButton.filledTonal(
+                      onPressed: () {
+                        bookDetailProv.likeBook(booksDetailData!);
+                      },
+                      icon: Icon(
+                        likes ? Icons.favorite : Icons.favorite_border,
+                        color: likes ? Colors.redAccent : null,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        } else {
+          return SizedBox.shrink();
+        }
+      },
+    );
+  }
+}
