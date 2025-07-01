d7ab475adf535f564f2ffc8f2b2e1e19157d58f2
clusterDe <dhannydhinata@gmail.com>
2025-07-01 23:20:03 +0700

    update like screen

--- lib/screens/likes/likes_screen.dart
+++ lib/screens/likes/likes_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'components/likes_body.dart';
 
 class LikesScreen extends StatelessWidget {
   static const String routeName = '/likes';
@@ -6,6 +7,9 @@ class LikesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Likes Book'), centerTitle: true),
+      body: LikesBody(),
+    );
   }
 }
