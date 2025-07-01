220df8872ede280b7f1fa7bf335b8a4f550f7fdd
clusterDe <dhannydhinata@gmail.com>
2025-07-01 23:20:33 +0700

    update route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:palm_mobile/providers/books_detail/books_detail_provider.dart';
 import 'package:palm_mobile/providers/home/home_provider.dart';
+import 'package:palm_mobile/providers/likes/likes_provider.dart';
+import 'package:palm_mobile/screens/books_detail/books_detail_screen.dart';
 import 'package:palm_mobile/screens/dashboard/dashboard_screen.dart';
 import 'package:palm_mobile/screens/home/home_screen.dart';
 import 'package:palm_mobile/screens/likes/likes_screen.dart';
 import 'package:provider/provider.dart';
+
+import '../models/books/components/books_result.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
@@ -14,33 +19,38 @@ GoRouter route = GoRouter(
   initialLocation: HomeScreen.routeName,
   restorationScopeId: HomeScreen.routeName,
   routes: [
-    StatefulShellRoute.indexedStack(
-      builder: (context, state, navigationShell) {
-        return DashboardScreen(navigationShell: navigationShell);
+    ShellRoute(
+      builder: (context, state, child) {
+        return DashboardScreen(navigationShell: child, key: state.pageKey);
       },
-      branches: [
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: HomeScreen.routeName,
-              name: HomeScreen.routeName,
-              builder: (context, state) => ChangeNotifierProvider.value(
-                value: HomeProvider(),
-                child: HomeScreen(),
-              ),
-            ),
-          ],
+      routes: [
+        GoRoute(
+          path: HomeScreen.routeName,
+          name: HomeScreen.routeName,
+          builder: (context, state) => ChangeNotifierProvider.value(
+            value: HomeProvider(),
+            child: HomeScreen(key: state.pageKey),
+          ),
         ),
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: LikesScreen.routeName,
-              name: LikesScreen.routeName,
-              builder: (context, state) => LikesScreen(),
-            ),
-          ],
+        GoRoute(
+          path: LikesScreen.routeName,
+          name: LikesScreen.routeName,
+          builder: (context, state) => ChangeNotifierProvider.value(
+            value: LikesProvider(),
+            child: LikesScreen(key: state.pageKey),
+          ),
         ),
       ],
+    ),
+    GoRoute(
+      path: BooksDetailScreen.routeName,
+      name: BooksDetailScreen.routeName,
+      builder: (context, state) {
+        return ChangeNotifierProvider.value(
+          value: BooksDetailProvider(booksData: state.extra as BooksResult),
+          child: BooksDetailScreen(),
+        );
+      },
     ),
   ],
 );
